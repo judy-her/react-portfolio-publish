1d42d66857af91ccac0c6b4664a2d2d236655398
judy-her <judith85dm@ucla.edu>
2024-04-03 13:41:34 -0700

    changed footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
       <div className="footer-box bg-black">
-        <h3 className="footer footer-gradient">© 2023 judysDreams, Inc</h3>
+        <h3 className="footer footer-gradient">© 2024 judysDreams, Inc</h3>
       </div>
     </footer>
   );
